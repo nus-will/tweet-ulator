3ec37624cd1b26d212451db4b6c53178bbb3d509
nus-will <will@nustechnology.com>
2022-01-19 14:07:16 +0700

    Add access control allow origin for API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,12 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
+app.use( function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use('/api', router);
 console.log(process.env.DB_URL)
